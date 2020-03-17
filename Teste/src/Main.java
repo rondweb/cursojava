5d38cf41037f9c10c1e91ae6a14f9fbad134656f
higorsantos <higorsantos@localhost>
2020-03-16 20:18:17 -0400

    Teste 1 mac

---
+++ Teste/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i <= 5; i++) {
+			System.out.println("laranjas");
+
+			for (int j = 0; j <= 3; j++) {
+
+				System.out.println("macas");
+
+				
+			}
+
+		}
+	}
+
+}
