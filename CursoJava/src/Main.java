5ee21601ee9b2565b970baf02cf5b5ec7f169650
ronaldo <rondweb@gmail.com>
2020-03-16 20:46:46 -0400

    Variavel de Cultura

--- CursoJava/src/Main.java
+++ CursoJava/src/Main.java
@@ -1,9 +1,14 @@
+import java.util.Locale;
 
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-
+		double f = 2.34;
+		
+		System.out.println(f);
+		
+		Locale.setDefault(Locale.US); //criando variavel do tipo cultura US
 	}
 
 }
