653062dec9efc86e68c7b4144dbee59bcb132566
ronaldo <rondweb@gmail.com>
2020-03-17 21:39:19 -0400

    Funcoes matematicas em java

---
+++ CursoJava/src/Matematica.java
@@ -0,0 +1,15 @@
+
+public class Matematica {
+
+	public static void main(String[] args) {
+		double A = Math.sqrt(25); //raiz quadrada
+		double B = Math.pow(5, 3); //potencia
+		double C = Math.abs(-5); //valor absoluto
+		
+		System.out.println("Raiz quadrada " + A);
+		System.out.println("Potencia de 5 elevado a 3 " + B);
+		System.out.println("Valor absoluto de -5 "  + C);
+
+	}
+
+}
